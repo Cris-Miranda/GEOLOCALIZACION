fef623f1b915bf14cadf5a71fe57f84332c7a8aa
Cris Miranda <cristianmirandapadilla003@gmail.com>
2024-09-21 23:13:00 -0600

    Establecemos las dimenciones de la figura

---
+++ visualizacion_terraquea.py
@@ -0,0 +1,10 @@
+from mpl_toolkits.basemap import Basemap
+import matplotlib.pyplot as plt 
+import numpy as np 
+# Latitud y longitud de varios puntos de interes
+lat = [19.4326009, -34.9964962, 38.89511]
+lon = [-99.1333415, -64.9672816, -77.03637]
+# Inicializamos las dimenciones de la figura
+plt.figure (figsize = (16, 12))
+
+
