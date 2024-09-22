b1f533b44c450d482b1bcfdce1c247160052e844
Cris Miranda <cristianmirandapadilla003@gmail.com>
2024-09-21 18:25:35 -0600

    Inicio del codigo

---
+++ geolocalizacion.py
@@ -0,0 +1,8 @@
+# Importamos el modulo time
+import time
+from geopy.geocoders import Nominatim
+# Generamos un objeto de la clase Nominatim
+geolocator = Nominatim()
+# Creamos una lista de lugares a buscar
+places = ["Mexico", "Buenos Aires Argentina", "175 5th Avenue NYC"]
+# Utilizamos la funcion geocode
